c228fe504ab27c1383375e1340119dfd242cbe9a
Kalcifer <7829231+K4LCIFER@users.noreply.github.com>
2021-08-16 23:52:43 -0700

    Added the reuse_tx_pl function

--- nrf24l01.py
+++ nrf24l01.py
@@ -475,7 +475,23 @@ def fulsh_rx():
 
 
 def reuse_tx_pl():
-    return
+    # UART data:
+    command_length = 1  # 1 command byte
+    transfer_length = 1 # 1 command byte
+    response_length = 1 # 1 status byte
+    with serial.Serial(PORT, BAUD, timeout=1) as ser:
+        # Transmit the UART command length header
+        ser.write(command_length.to_bytes(1, 'big'))
+        # Transmit the SPI transfer length header
+        ser.write(transfer_length.to_bytes(1, 'big'))
+        # Transmit the command byte
+        ser.write(COMMANDS['REUSE_TX_PL'].to_bytes(1, 'big'))
+        # Read and return the UART response
+        uart_response = ser.read(response_length)
+        uart_response_formatted = {}
+        uart_response_formatted['RAW'] = uart_response
+        uart_response_formatted['STATUS'] = uart_response[0].to_bytes(1, 'big')
+    return uart_response_formatted
 
 
 def r_rx_pl_wid():
