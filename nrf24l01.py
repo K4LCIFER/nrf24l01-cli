f0935b6002361f33a99a66eba123085d148fb74e
Kalcifer <7829231+K4LCIFER@users.noreply.github.com>
2021-08-16 21:15:48 -0700

    Added and documented the w_register function.

--- nrf24l01.py
+++ nrf24l01.py
@@ -27,7 +27,6 @@ import serial
 import platform
 
 
-
 BAUD = 9600
 if platform.system() == 'Linux':
     PORT = '/dev/ttyUSB0'
@@ -64,7 +63,7 @@ COMMANDS = {
 REGISTER_MAP = {
     'CONFIG': {
         'ADDRESS': 0x00,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'MASK_RX_DR': {'LENGTH': 1, 'OFFSET': 6},
         'MASK_TX_DS': {'LENGTH': 1, 'OFFSET': 5},
         'MASK_MAX_RT': {'LENGTH': 1, 'OFFSET': 4},
@@ -75,7 +74,7 @@ REGISTER_MAP = {
     },
     'EN_AA': {
         'ADDRESS': 0x01,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'ENAA_P5': {'LENGTH': 1, 'OFFSET': 5},
         'ENAA_P4': {'LENGTH': 1, 'OFFSET': 4},
         'ENAA_P3': {'LENGTH': 1, 'OFFSET': 3},
@@ -85,7 +84,7 @@ REGISTER_MAP = {
     },
     'EN_RXADDR': {
         'ADDRESS': 0x02,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'ERX_P5': {'LENGTH': 1, 'OFFSET': 5},
         'ERX_P4': {'LENGTH': 1, 'OFFSET': 4},
         'ERX_P3': {'LENGTH': 1, 'OFFSET': 3},
@@ -95,23 +94,23 @@ REGISTER_MAP = {
     },
     'SETUP_AW': {
         'ADDRESS': 0x03,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'AW': {'LENGTH': 2, 'OFFSET': 0},
     },
     'SETUP_RETR': {
         'ADDRESS': 0x04,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'ARD': {'LENGTH': 4, 'OFFSET': 4},
         'ARC': {'LENGTH': 4, 'OFFSET': 0},
     },
     'RF_CH': {
         'ADDRESS': 0x05,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'RF_CH': {'LENGTH': 7, 'OFFSET': 0},
     },
     'RF_SETUP': {
         'ADDRESS': 0x06,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'CONT_WAVE': {'LENGTH': 1, 'OFFSET': 7},
         'RF_DR_LOW': {'LENGTH': 1, 'OFFSET': 5},
         'PLL_LOCK': {'LENGTH': 1, 'OFFSET': 4},
@@ -120,7 +119,7 @@ REGISTER_MAP = {
     },
     'STATUS': {
         'ADDRESS': 0x07,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'RX_DR': {'LENGTH': 1, 'OFFSET': 6},
         'TX_DS': {'LENGTH': 1, 'OFFSET': 5},
         'MAX_RT': {'LENGTH': 1, 'OFFSET': 4},
@@ -128,74 +127,74 @@ REGISTER_MAP = {
         'TX_FULL': {'LENGTH': 1, 'OFFSET': 0},
     },
     'OBSERVE_TX': {
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'ADDRESS': 0x08,
         'PLOS_CNT': {'LENGTH': 4, 'OFFSET': 4},
         'ARC_CNT': {'LENGTH': 4, 'OFFSET': 0},
     },
     'RPD': {
         'ADDRESS': 0x09,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'RPD': {'LENGTH': 1, 'OFFSET': 0},
     },  # RPD or CD?
     'RX_ADDR_P0': {
         'ADDRESS': 0x0A,
-        'NUMBER_OF_BYTES': 5,
+        'NUMBER_OF_DATA_BYTES': 5,
     },
     'RX_ADDR_P1': {
         'ADDRESS': 0x0B,
-        'NUMBER_OF_BYTES': 5,
+        'NUMBER_OF_DATA_BYTES': 5,
     },
     'RX_ADDR_P2': {
         'ADDRESS': 0x0C,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
     },
     'RX_ADDR_P3': {
         'ADDRESS': 0x0D,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
     },
     'RX_ADDR_P4': {
         'ADDRESS': 0x0E,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
     },
     'RX_ADDR_P5': {
         'ADDRESS': 0x0F,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
     },
-    'TX_ADDR': {'ADDRESS': 0x10, 'NUMBER_OF_BYTES': 5},
+    'TX_ADDR': {'ADDRESS': 0x10, 'NUMBER_OF_DATA_BYTES': 5},
     'RX_PW_P0': {
         'ADDRESS': 0x11,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'RX_PW_P0': {'LENGTH': 6, 'OFFSET': 0},
     },
     'RX_PW_P1': {
         'ADDRESS': 0x12,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'RX_PW_P1': {'LENGTH': 6, 'OFFSET': 0},
     },
     'RX_PW_P2': {
         'ADDRESS': 0x13,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'RX_PW_P2': {'LENGTH': 6, 'OFFSET': 0},
     },
     'RX_PW_P3': {
         'ADDRESS': 0x14,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'RX_PW_P3': {'LENGTH': 6, 'OFFSET': 0},
     },
     'RX_PW_P4': {
         'ADDRESS': 0x15,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'RX_PW_P4': {'LENGTH': 6, 'OFFSET': 0},
     },
     'RX_PW_P5': {
         'ADDRESS': 0x16,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'RX_PW_P5': {'LENGTH': 6, 'OFFSET': 0},
     },
     'FIFO_STATUS': {
         'ADDRESS': 0x17,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'TX_REUSE': {'LENGTH': 1, 'OFFSET': 6},
         'TX_FULL': {'LENGTH': 1, 'OFFSET': 5},
         'TX_EMPTY': {'LENGTH': 1, 'OFFSET': 4},
@@ -204,7 +203,7 @@ REGISTER_MAP = {
     },
     'DYNPD': {
         'ADDRESS': 0x1C,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'DPL_P5': {'LENGTH': 1, 'OFFSET': 5},
         'DPL_P4': {'LENGTH': 1, 'OFFSET': 4},
         'DPL_P3': {'LENGTH': 1, 'OFFSET': 3},
@@ -214,7 +213,7 @@ REGISTER_MAP = {
     },
     'FEATURE': {
         'ADDRESS': 0x1D,
-        'NUMBER_OF_BYTES': 1,
+        'NUMBER_OF_DATA_BYTES': 1,
         'EN_DPL': {'LENGTH': 1, 'OFFSET': 2},
         'EN_ACK_PAY': {'LENGTH': 1, 'OFFSET': 1},
         'EN_DYN_ACK': {'LENGTH': 1, 'OFFSET': 0},
@@ -279,7 +278,7 @@ def r_register(register_name):
         # Specify the length of the requested data associated with the
         # command word + 1 for the status register.
         ser.write(
-            (REGISTER_MAP[register_name]['NUMBER_OF_BYTES'] + 1).to_bytes(
+            (REGISTER_MAP[register_name]['NUMBER_OF_DATA_BYTES'] + 1).to_bytes(
                 1, 'big'
             )
         )
@@ -294,27 +293,28 @@ def r_register(register_name):
         # Read the data returned over UART.
         uart_response = int.from_bytes(
             ser.read(
-                REGISTER_MAP[register_name]['NUMBER_OF_BYTES'] + 1,
+                REGISTER_MAP[register_name]['NUMBER_OF_DATA_BYTES'] + 1,
             ),
             'big',
         )
         # Save an entry for the raw returned data.
         uart_response_formatted['RAW'] = uart_response
+        # TODO: Add the returned status register to the returned dictionary.
         # The following is a special case of the registers in the nRF24L01. If
         # a register has only one byte associated with it, then it will always
         # have bit mnemonics describing the bits that are at the specified
         # register adderss; however, if the register containts more than one
         # byte, then it does not have any bit mnemonics describing the bit
         # positions of the register, and instead has whole chunks of data.
-        if REGISTER_MAP[register_name]['NUMBER_OF_BYTES'] == 1:
+        if REGISTER_MAP[register_name]['NUMBER_OF_DATA_BYTES'] == 1:
             # First collect the bit mnemonic(s) into a dictionary and assign
             # each of them their respecive returned value.
             uart_response_bit_mnemonics = {}
             for bit_mnemonic in REGISTER_MAP[register_name]:
-                # Ignore the ADDRESS and NUMBER_OF_BYTES keys.
+                # Ignore the ADDRESS and NUMBER_OF_DATA_BYTES keys.
                 if (
                     bit_mnemonic != 'ADDRESS'
-                    and bit_mnemonic != 'NUMBER_OF_BYTES'
+                    and bit_mnemonic != 'NUMBER_OF_DATA_BYTES'
                 ):
                     # Extract the respective bit menmonics bit value from the
                     # raw received data and write it to the dictionary.
@@ -329,24 +329,58 @@ def r_register(register_name):
                     uart_response_formatted[
                         register_name
                     ] = uart_response_bit_mnemonics
-        elif REGISTER_MAP[register_name]['NUMBER_OF_BYTES'] > 1:
+        elif REGISTER_MAP[register_name]['NUMBER_OF_DATA_BYTES'] > 1:
             # Dump the total received data into the dictionary.
             uart_response_formatted[register_name] = uart_response
         return uart_response_formatted
 
 
 def w_register(register_name, payload):
+    # NOTE: Might change this stuff.
+    if type(payload) == list:
+        if len(payload) != REGISTER_MAP[register_name]['NUMBER_OF_DATA_BYTES']:
+            raise ValueError("Incorrect payload length!")
+    if type(payload) == int:
+        if payload > 255:
+            raise ValueError("")
     with serial.Serial(PORT, BAUD, timeout=1) as ser:
+        # Send header bytes:
+        # Transmit data length - Command Byte + the number of Data Bytes.
+        ser.write(
+            (REGISTER_MAP[register_name]['NUMBER_OF_DATA_BYTES'] + 1).to_bytes(
+                1, 'big'
+            )
+        )
+        # NOTE: I don't like "Returned data length". It isn't overly descriptive
+        # of what it is/does. It should be more like number of transfer bytes,
+        # in reference to SPI, as it is for the spi; however it is also for the
+        # data returned to the UART sometimes. Some more thought is needed for
+        # this.
+        # Returned data length - Status Register + the number of Data Bytes.
+        ser.write(
+            (REGISTER_MAP[register_name]['NUMBER_OF_DATA_BYTES'] + 1).to_bytes(
+                1, 'big'
+            )
+        )
+        # Send command byte:
         ser.write(
             (
                 COMMANDS['W_REGISTER'] | REGISTER_MAP[register_name]['ADDRESS']
             ).to_bytes(1, 'big')
         )
-        # ser.write(b'\x0c')
-        for i in range(5):
-            ser.write(b'\x0c')
-            # ser.write((COMMANDS['NOP']).to_bytes(1, 'big'))
-        print(ser.read(6))
+        # Send the data bytes:
+        # TODO: Allow this statement to handle multiple types.
+        if isinstance(payload, list):
+            for byte in payload:
+                ser.write(byte.to_bytes(1, 'big'))
+        elif isinstance(payload, int):
+            ser.write(payload.to_bytes(1, 'big'))
+        # Read and return the status register.
+        # TODO: Add the status register to the returned data dictionary.
+        uart_response = int.from_bytes(ser.read(1), 'big')
+        uart_response_formatted = {}
+        uart_response_formatted['RAW'] = uart_response
+        return uart_response_formatted
 
 
 def r_rx_payload():
